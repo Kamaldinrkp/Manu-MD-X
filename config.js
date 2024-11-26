d63d5b5a76761f2221c49d4522c01cf0f44419b9
Kamaldinrkp <kamaldinrk02@gmail.com>
2024-11-26 20:50:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&ucNRXRiQ#xxBBDlUlua2Y7u-MVbvepOihLn7xx3BV7IBuwisnZvg"
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&DRtXFArT#PH1SUiin8hq1ql254OWyTaPm4UI_mgBVJgJZKmMCRxc"
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
